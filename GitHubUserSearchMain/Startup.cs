baa9248f83ad5b8e75cfc3c4ad99f7b0fcbb15ab
Owen Thomas <s3496293@student.rmit.edu.au>
2018-11-22 12:43:49 +1100

    Add project files.

---
+++ GitHubUserSearchMain/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubUserSearchMain.Startup))]
+namespace GitHubUserSearchMain
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
